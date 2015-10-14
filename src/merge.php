80f1ff143dda0c564c7db2e71cd6ea606f4c7709
Kei <kei.of.nakayama@gmail.com>
2015-10-14 15:28:32 +0900

    created src

--- src/merge.php
+++ src/merge.php
@@ -13,10 +13,11 @@ use ZendPdf\PdfDocument;
 use ZendPdf\Exception as PdfException;
 
 if (count($argv) < 3) {
-    echo "USAGE php $argv[0] <pdf_file1> [<pdf_file2> [<pdf_file3>[...]]] <output_pdf_file>";
+    echo "USAGE php $argv[0] <pdf_file1> <pdf_file2> [<pdf_file3>[...]] <output_pdf_file>";
     exit;
 }
 
+$pdfs = [];
 for ($i = 1; $i <= (count($argv) - 2); $i++) {
     echo "loading: " . $argv[$i] . PHP_EOL;
     $pdfs[] = PdfDocument::load($argv[$i]);
